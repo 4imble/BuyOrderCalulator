6d15908d866538718ed61ac5d4b7336e2aa17d80
Simon Lawrence <simon.lawrence@rsmuk.com>
2020-07-16 13:36:51 +0100

    development environment configured
    Typescript,
    CSS modules (new to me, css intellisense anyone?)
    Ant Design
    Less support
    Prevent browser window opening on start

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,9 +1,19 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
-    style: 'css'
+    style: true
+  }),
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: {
+        // "@primary-color": "#222", // customize as needed
+        // "@link-color": "#222", // customize as needed
+        // "@font-size-base": "18px", // customize as needed
+      },
+    },
   })
 );
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Button } from 'antd';
 import './App.less';
+import styles from './site.module.less';
 
 export default class App extends React.Component {
   render() {
     return (
-      <main
-        style={{
-          height: '500px',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
+      <main className={styles.app}>
         <Button type="primary">Hello, Ant Design!</Button>
         <a href="foo.bar">I'm a link. Click me, please!</a>
+        <span className={styles.gimble}>Gimble</span>
       </main>
     )
   };
