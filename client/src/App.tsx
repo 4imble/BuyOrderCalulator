c44c8948347b24516b53ad0068b9cbf2c4c98165
Simon Lawrence <simon.lawrence@rsmuk.com>
2020-07-17 15:14:13 +0100

    random hex on screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import './App.less';
 import styles from './site.module.less';
+
+const { Header, Footer, Content } = Layout;
 
 export default class App extends React.Component {
   render() {
     return (
-      <main className={styles.app}>
-        <Button type="primary">Hello, Ant Design!</Button>
-        <a href="foo.bar">I'm a link. Click me, please!</a>
-        <span className={styles.gimble}>Gimble</span>
-      </main>
+      <main>
+        <Layout>
+          <Header>Header</Header>
+          <Content style={{ padding: '50px' }}>
+            <Row>
+              <Col>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="300">
+                  <polygon className={styles.hex} points="300,150, 225,280 75,280 0,150 75,20 225,20"></polygon>
+                </svg>
+              </Col>
+            </Row>
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </main >
     )
   };
 }
