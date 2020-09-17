a64468a396791e671f7decb63baa8ef63c546843
Simon Lawrence <simon.lawrence@rsmuk.com>
2020-09-17 11:20:16 +0100

    now with filter box

--- Code/BuyOrderCalulator.Web.Client/src/pages/lobby/Lobby.tsx
+++ Code/BuyOrderCalulator.Web.Client/src/pages/lobby/Lobby.tsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Card, Button, Row, Col } from 'antd';
+import { Card, Button, Row, Col, Input } from 'antd';
 import { Item } from '../../domain/domain';
 
 export default function Lobby(props: any) {
     const history = useHistory();
-
+    const [allItems, setAllItems] = useState<Item[]>([]);
     const [items, setItems] = useState<Item[]>([]);
+    const [search, setSearch] = useState<string>("");
 
     useEffect(() => {
         fetchItems();
@@ -14,11 +15,15 @@ export default function Lobby(props: any) {
 
     async function fetchItems() {
         const result = await fetch("api/items");
-        result.json().then(res => setItems(res))
+        result.json().then(res => setAllItems(res))
             .catch(err => console.log(err));
     }
 
-    let itemRows = items.map((item, index) => <Row key={index}>
+    function filteredItems(): Array<Item> {
+        return allItems.filter(x => x.name.toUpperCase().startsWith(search.toUpperCase()))
+    }
+
+    let itemRows = filteredItems().map((item, index) => <Row key={index}>
         <Col className="gutter-row" flex="auto"></Col>
         <Col className="gutter-row">
             {item.name}
@@ -28,6 +33,9 @@ export default function Lobby(props: any) {
 
 
     return (
-        <div>{itemRows}</div>
+        <div>
+            <Input placeholder="Name" value={search} onChange={e => setSearch(e.target.value)} />
+            {itemRows}
+        </div>
     )
 }
